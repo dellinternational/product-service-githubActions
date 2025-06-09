c9e016103a63fe473d2b7e01570016e39758b313
Sudarshan <Sudarshan1994@yahoo.com>
2025-06-09 13:07:10 +0530

    hiiii

--- src/main/java/com/example/productservice/controller/ProductController.java
+++ src/main/java/com/example/productservice/controller/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductRepository repository;
